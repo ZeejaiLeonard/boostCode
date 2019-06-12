0a8981c4dafe827e2ef648dd49bcbb1bf25fa4e5
Zeejai Leonard <793313@seq.org>
2019-06-12 11:05:26 -0700

    tryna to find out how builtin function animate()?

--- bouncingAnimals.py
+++ bouncingAnimals.py
@@ -1,1 +1,19 @@
 # Write your code here :-)
+WIDTH = 800
+HEIGHT = 500
+
+animal1 = Actor('giraffe')
+animal1.pos = 200, 200
+animate(animal1, tween = "linear", duration = 5, on_finished = None, animal1.pos)
+
+def draw():
+    screen.clear()
+    animal1.draw()
+
+def on_mouse_down(pos):
+    if animal1.collidepoint(pos):
+        if animal1.image == "giraffe":
+            animal1.image = "dog"
+        elif animal1.image == "dog":
+            animal1.image = "giraffe"
+
